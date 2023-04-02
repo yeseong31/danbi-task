3d82559789359e9cf13adcad205e486031cc8234
yeseong31 <yeseong31@naver.com>
2023-04-02 20:42:45 +0900

    fix: team field를 ManyToMany로 변경

--- task/models.py
+++ task/models.py
@@ -8,7 +8,7 @@ from account.models import User, Team
 class Task(models.Model):
     id = models.AutoField(primary_key=True)
     create_user = models.ForeignKey(User, on_delete=models.CASCADE, null=False, blank=False, verbose_name='작성자')
-    team = models.ForeignKey(Team, on_delete=models.CASCADE, null=False, blank=False, verbose_name='업무 진행 팀')
+    team = models.ManyToManyField(Team, null=False, blank=False, verbose_name='업무 진행 팀', related_name='task_team')
     title = models.CharField(max_length=255, null=False, blank=False, verbose_name='업무명')
     content = models.TextField(null=True, blank=True, verbose_name='업무 내용')
     is_complete = models.BooleanField(default=False, verbose_name='업무 완료 여부')
@@ -25,7 +25,7 @@ class Task(models.Model):
 
 class SubTask(models.Model):
     id = models.AutoField(primary_key=True)
-    team = models.ForeignKey(Team, on_delete=models.CASCADE, null=False, blank=False, verbose_name='하위업무 진행 팀')
+    team = models.ManyToManyField(Team, null=False, blank=False, verbose_name='하위업무 진행 팀', related_name='subtask_team')
     is_complete = models.BooleanField(default=False, verbose_name='하위업무 완료 여부')
     completed_date = models.DateTimeField(null=True, blank=True, verbose_name='하위업무 완료일')
     created_at = models.DateTimeField(default=datetime.now(), null=False, blank=False, verbose_name='하위업무 생성일')
