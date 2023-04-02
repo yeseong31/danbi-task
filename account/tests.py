d4e95148663582f7a03c8bb6d115164bfb7e8d6b
yeseong31 <yeseong31@naver.com>
2023-04-02 16:55:44 +0900

    test: APITestCase test

--- account/tests.py
+++ account/tests.py
@@ -1,17 +1,36 @@
 from django.test import TestCase
+from rest_framework.test import APIRequestFactory, APITestCase
 
-from account.models import User
+from account.models import User, Team
 
 
-class UserModelTest(TestCase):
+class TeamModelTest(TestCase):
     def test_default_values(self):
-        user = User.objects.create(email='test@test.com',
-                                   username='test_nickname_1',
-                                   team=None,
-                                   password='!test@1234?')
-        self.assertEqual(user.email, 'test@test.com')
-        self.assertEqual(user.username, 'test_nickname_1')
-        self.assertEqual(user.password, '!test@1234?')
-        self.assertEqual(user.is_active, True)
-        self.assertEqual(user.is_admin, False)
+        team = Team.objects.create(
+            name='테스트팀'
+        )
+        self.assertEqual(team.name, '테스트팀')
 
+
+class UserModelTest(APITestCase):
+    def setUp(self):
+        self.factory = APIRequestFactory()
+        self.register_url = '/auth/signup/'
+        self.login_url = '/auth/login/'
+        self.team = Team.objects.create(
+            name='테스트팀'
+        )
+        self.user = User.objects.create_user(
+            email='user1@test.com',
+            username='user1',
+            team=self.team,
+            pw='!test@1234?',
+        )
+
+    def test_default_values(self):
+        self.assertEqual(self.user.email, 'user1@test.com')
+        self.assertEqual(self.user.username, 'user1')
+        self.assertEqual(self.user.team, self.team)
+        self.assertEqual(self.user.check_password('!test@1234?'), True)
+        self.assertEqual(self.user.is_active, True)
+        self.assertEqual(self.user.is_admin, False)
