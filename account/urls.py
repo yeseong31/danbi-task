1b59f9e77de2f8b313d9408024d641347d5419c5
yeseong31 <yeseong31@naver.com>
2023-04-02 16:56:35 +0900

    chore: 회원 가입 test

---
+++ account/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from account.views import RegisterView
+
+app_name = 'auth'
+
+urlpatterns = [
+    # path('signup/', RegisterView.as_view(), name='signup'),
+]
--- account/views.py
+++ account/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from account.models import User
+from account.serializers import RegisterSerializer
+
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = RegisterSerializer
