7ef0ab41cd4f593269e18060e6069e5268e03637
yeseong31 <yeseong31@naver.com>
2023-04-02 14:28:40 +0900

    remove: dev.py

--- config/settings/dev.py
+++
@@ -1,9 +0,0 @@
-from .base import *
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
