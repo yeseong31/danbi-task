3c4cb64619795eb7ccd50220f90cbfb94f2ca52b
yeseong31 <yeseong31@naver.com>
2023-04-02 15:58:27 +0900

    test: User model test

--- account/tests.py
+++ account/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
 
-# Create your tests here.
+from account.models import User
+
+
+class UserModelTest(TestCase):
+    def test_default_values(self):
+        user = User.objects.create(email='test@test.com',
+                                   username='test_nickname_1',
+                                   team=None,
+                                   password='!test@1234?')
+        self.assertEqual(user.email, 'test@test.com')
+        self.assertEqual(user.username, 'test_nickname_1')
+        self.assertEqual(user.password, '!test@1234?')
+        self.assertEqual(user.is_active, True)
+        self.assertEqual(user.is_admin, False)
+
