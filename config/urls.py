5e6e3fec50655d03cbaaf968de595d16243f4d7b
yeseong31 <yeseong31@naver.com>
2023-04-03 11:51:03 +0900

    feat: 홈 url의 redirect 추가

--- config/urls.py
+++ config/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('task.urls')),
+    path('', RedirectView.as_view(url='/task/', permanent=True)),
+    path('task/', include('task.urls')),
     # path('auth/', include('account.urls')),
 ]
