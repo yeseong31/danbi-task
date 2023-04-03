05db74289fc5700e7461e6b73a22399f19087604
yeseong31 <yeseong31@naver.com>
2023-04-03 21:48:20 +0900

    chore: 회원가입 및 로그인 view 이름 수정
    - Token Refresh URL 추가

--- account/urls.py
+++ account/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenRefreshView
 
-from account.views import RegisterView, LoginView
+from account.views import RegisterAPIView, LoginAPIView
 
 app_name = 'auth'
 
 urlpatterns = [
-    path('signup/', RegisterView.as_view(), name='signup'),
-    path('login/', LoginView.as_view(), name='login'),
+    path('signup/', RegisterAPIView.as_view(), name='signup'),
+    path('login/', LoginAPIView.as_view(), name='login'),
+    path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
