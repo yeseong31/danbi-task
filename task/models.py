5548d66280cbe6571891aa90e9ece14821b6d51b
yeseong31 <yeseong31@naver.com>
2023-04-03 22:11:11 +0900

    chore: verbose_name과 field name 통합

--- task/models.py
+++ task/models.py
@@ -7,14 +7,14 @@ from account.models import User, Team
 
 class Task(models.Model):
     id = models.AutoField(primary_key=True)
-    create_user = models.ForeignKey(User, on_delete=models.CASCADE, null=False, blank=False, verbose_name='작성자')
-    team = models.ManyToManyField(Team, null=False, blank=False, verbose_name='업무 진행 팀', related_name='task_team')
-    title = models.CharField(max_length=255, null=False, blank=False, verbose_name='업무명')
-    content = models.TextField(null=True, blank=True, verbose_name='업무 내용')
-    is_complete = models.BooleanField(default=False, verbose_name='업무 완료 여부')
-    completed_date = models.DateTimeField(null=True, blank=True, verbose_name='업무 완료일')
-    created_at = models.DateTimeField(default=datetime.now(), null=False, blank=False, verbose_name='업무 생성일')
-    modified_at = models.DateTimeField(null=True, blank=True, verbose_name='업무 수정일')
+    create_user = models.ForeignKey(User, on_delete=models.CASCADE, null=False, blank=False, verbose_name='create_user')
+    team = models.ManyToManyField(Team, null=False, blank=False, verbose_name='team', related_name='task_team')
+    title = models.CharField(max_length=255, null=False, blank=False, verbose_name='title')
+    content = models.TextField(null=True, blank=True, verbose_name='content')
+    is_complete = models.BooleanField(default=False, verbose_name='is_complete')
+    completed_date = models.DateTimeField(null=True, blank=True, verbose_name='completed_date')
+    created_at = models.DateTimeField(default=datetime.now(), null=False, blank=False, verbose_name='created_at')
+    modified_at = models.DateTimeField(null=True, blank=True, verbose_name='modified_at')
 
     def __str__(self):
         return f'[Task {self.id}] {self.title}'
@@ -25,11 +25,11 @@ class Task(models.Model):
 
 class SubTask(models.Model):
     id = models.AutoField(primary_key=True)
-    team = models.ManyToManyField(Team, null=False, blank=False, verbose_name='하위업무 진행 팀', related_name='subtask_team')
-    is_complete = models.BooleanField(default=False, verbose_name='하위업무 완료 여부')
-    completed_date = models.DateTimeField(null=True, blank=True, verbose_name='하위업무 완료일')
-    created_at = models.DateTimeField(default=datetime.now(), null=False, blank=False, verbose_name='하위업무 생성일')
-    modified_at = models.DateTimeField(null=True, blank=True, verbose_name='하위업무 수정일')
+    team = models.ManyToManyField(Team, null=False, blank=False, verbose_name='team', related_name='subtask_team')
+    is_complete = models.BooleanField(default=False, verbose_name='is_complete')
+    completed_date = models.DateTimeField(null=True, blank=True, verbose_name='completed_date')
+    created_at = models.DateTimeField(default=datetime.now(), null=False, blank=False, verbose_name='created_at')
+    modified_at = models.DateTimeField(null=True, blank=True, verbose_name='modified_at')
 
     def __str__(self):
         return f'[SubTask {self.id}]'
