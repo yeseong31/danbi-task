66bdb4593709d1565bdc683c29a19792778ace71
yeseong31 <yeseong31@naver.com>
2023-04-03 23:10:08 +0900

    feat: Task 목록 및 상세 serializer 추가

--- task/serializers.py
+++ task/serializers.py
@@ -5,28 +5,35 @@ from task.models import Task, SubTask
 
 
 class SubTaskSerializer(serializers.ModelSerializer):
-    team = TeamSerializer(read_only=True, many=True)
+    team = TeamSerializer(read_only=True)
 
     class Meta:
         model = SubTask
         fields = ('id', 'team', 'is_complete',
                   'completed_date', 'created_at', 'modified_at',)
+        
 
-    @classmethod
-    def setup_preloading(cls, queryset):
-        return queryset.select_related('team')
-
-
-class TaskSerializer(serializers.ModelSerializer):
-    team = TeamSerializer(read_only=True, many=True)
+class TaskListSerializer(serializers.ModelSerializer):
+    team = TeamSerializer(read_only=True)
     create_user = UserProfileSerializer(read_only=True)
-
+    
     class Meta:
         model = Task
         fields = ('id', 'create_user', 'team',
                   'title', 'content', 'is_complete',
                   'completed_date', 'created_at', 'modified_at',)
 
+
+class TaskDetailSerializer(serializers.ModelSerializer):
+    team = TeamSerializer(read_only=True)
+    create_user = UserProfileSerializer(read_only=True)
+    
+    class Meta:
+        model = Task
+        fields = ('id', 'create_user', 'team',
+                  'title', 'content', 'is_complete',
+                  'completed_date', 'created_at', 'modified_at',)
+    
     @classmethod
     def setup_preloading(cls, queryset):
         return queryset.select_related('team')
