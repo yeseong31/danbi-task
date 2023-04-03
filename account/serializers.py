6802ff814ad30e604f9dc57113e7f548dcc0ce48
yeseong31 <yeseong31@naver.com>
2023-04-03 21:46:56 +0900

    refactor: User 관련 Serializer 수정

--- account/serializers.py
+++ account/serializers.py
@@ -1,39 +1,20 @@
-from django.contrib.auth import authenticate, get_user_model
 from django.contrib.auth.password_validation import validate_password
 from rest_framework import serializers
-from rest_framework.authtoken.models import Token
-from rest_framework.generics import get_object_or_404
-from rest_framework.validators import UniqueValidator
 
-from .models import User, Team
+from account.models import User
 
 
-class RegisterSerializer(serializers.ModelSerializer):
-    email = serializers.EmailField(
-        required=True,
-        validators=[UniqueValidator(queryset=User.objects.all())],  # 이메일 중복 검증
-    )
+class UserSerializer(serializers.ModelSerializer):
     pw = serializers.CharField(
         write_only=True,
         required=True,
-        validators=[validate_password],  # 비밀번호 검증
+        validators=[validate_password],
         help_text='비밀번호를 입력해 주세요.'
-    )
-    pw2 = serializers.CharField(
-        write_only=True,
-        required=True,
-        help_text='확인을 위해 다시 한 번 비밀번호를 입력해 주세요.'
     )
 
     class Meta:
         model = User
-        fields = ('email', 'username', 'team', 'pw', 'pw2',)
-
-    def validate(self, data):
-        """비밀번호 일치 여부 확인"""
-        if data['pw'] != data['pw2']:
-            raise serializers.ValidationError({'pw': "비밀번호가 일치하지 않습니다."})
-        return data
+        fields = ('email', 'username', 'team', 'pw',)
 
     def create(self, validated_data):
         user = User.objects.create_user(
@@ -43,28 +24,13 @@ class RegisterSerializer(serializers.ModelSerializer):
         )
         user.set_password(validated_data['pw'])
         user.save()
-        Token.objects.create(user=get_user_model().objects.first())
         return user
-    
 
-class LoginSerializer(serializers.Serializer):
+
+class LoginSerializer(serializers.ModelSerializer):
     email = serializers.CharField(required=True)
-    password = serializers.CharField(required=True, write_only=True)
+    pw = serializers.CharField(required=True, write_only=True, validators=[validate_password],)
 
-    def validate(self, data):
-        user = get_object_or_404(User, email=data['email'])
-        if user and user.check_password(data['password']):
-            return Token.objects.get(user=get_user_model().objects.first())
-        raise serializers.ValidationError({'error': '제공된 자격 증명으로 로그인할 수 없습니다.'})
-
-
-class UserProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('id', 'username', 'team',)
-
-
-class TeamSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Team
-        fields = ('id', 'name',)
+        fields = ('email', 'pw',)
