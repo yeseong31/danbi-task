923350b1d4e81d06ad160d8d014ab50c35a1249b
yeseong31 <yeseong31@naver.com>
2023-04-03 10:17:00 +0900

    feat: serializer 내용 추가

--- task/serializers.py
+++ task/serializers.py
@@ -1,11 +1,25 @@
 from rest_framework import serializers
 
-from task.models import Task
+from task.models import Task, SubTask
+
+
+class SubTaskSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SubTask
+        fields = ('id', 'username', 'team',)
 
 
 class TaskSerializer(serializers.ModelSerializer):
+    subtask = SubTaskSerializer(read_only=True)  # nested serializer
+
     class Meta:
         model = Task
-        fields = ['id', 'create_user', 'team',
+        fields = ('id', 'create_user', 'team',
                   'title', 'content', 'is_complete',
-                  'completed_date', 'created_at', 'modified_at']
+                  'completed_date', 'created_at', 'modified_at',)
+
+
+class TaskCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Task
+        fields = ('create_user', 'team', 'title', 'content',)
