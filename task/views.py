c86b3a38f5df149fef1cbfb8562a24f25975a6e3
yeseong31 <yeseong31@naver.com>
2023-04-03 11:34:51 +0900

    chore: Task와 SubTask를 분리하여 결과 반환

--- task/views.py
+++ task/views.py
@@ -8,10 +8,13 @@ from task.serializers import SubTaskSerializer, TaskSerializer
 
 class TasksAPI(APIView):
     def get(self, request):
-        """전체 Task 정보 조회"""
+        """전체 Task 목록 조회"""
         tasks = Task.objects.all()
         sub_tasks = SubTask.objects.all()  # 사용자가 속한 팀에 따라 결과 달라야 함
         serializer1 = TaskSerializer(tasks, many=True)
         serializer2 = SubTaskSerializer(sub_tasks, many=True)
-        data = sorted(serializer1.data + serializer2.data, key=lambda x: x['created_at'])
+        data = {
+            'tasks': serializer1.data,
+            'subTasks': serializer2.data
+        }
         return Response(data, status=status.HTTP_200_OK)
