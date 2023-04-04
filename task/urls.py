7425f11822841f72e54d225f0510c33864349dcc
yeseong31 <yeseong31@naver.com>
2023-04-04 16:08:08 +0900

    feat: SubTask 완료 처리 추가

--- task/urls.py
+++ task/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 
+from task.views.sub_task_views import sub_task_update
 from task.views.task_views import task_list, task_detail, task_create, task_update
 
 app_name = 'task'
@@ -9,4 +10,5 @@ urlpatterns = [
     path('detail/<int:pk>/', task_detail, name='task_detail'),
     path('create/', task_create, name='task_create'),
     path('update/<int:pk>/', task_update, name='task_update'),
+    path('update/sub/<int:pk>/', sub_task_update, name='sub_task_update'),
 ]
---
+++ task/views/sub_task_views.py
@@ -0,0 +1,69 @@
+import os
+from datetime import datetime
+
+import jwt
+from rest_framework import status
+from rest_framework.decorators import api_view, permission_classes, authentication_classes
+from rest_framework.generics import get_object_or_404
+from rest_framework.response import Response
+from rest_framework_simplejwt.authentication import JWTAuthentication
+
+from account.models import User
+from task.models import SubTask, Task
+from task.permissions import CustomReadOnly
+from task.serializers import SubTaskDetailSerializer
+
+
+@api_view(['PUT'])
+@permission_classes([CustomReadOnly])
+@authentication_classes([JWTAuthentication])
+def sub_task_update(request, pk):
+    """
+    SubTask 수정(완료 처리)
+    
+    :params:
+    - pk: SubTask pk(ID)
+    """
+    try:
+        access = request.headers.get('Authorization')
+
+        if access is None:
+            raise jwt.exceptions.ExpiredSignatureError
+        payload = jwt.decode(access.split()[1], os.getenv('SECRET_KEY'), algorithms=['HS256'])
+        user = get_object_or_404(User, pk=payload.get('user_id'))
+        sub_task = SubTask.objects.get(pk=pk)
+        
+        if user.team.id != sub_task.team.id:
+            return Response({'message': '하위업무를 생성한 팀애 속한 사용자만이 완료 처리를 할 수 있습니다.'},
+                            status=status.HTTP_401_UNAUTHORIZED)
+        if sub_task.is_complete:
+            return Response({'message': '이미 완료한 하위업무입니다.'},
+                            status=status.HTTP_400_BAD_REQUEST)
+
+        sub_task.is_complete = True
+        end_time = datetime.now()
+        sub_task.modified_at = end_time
+        sub_task.completed_date = end_time
+        sub_task.save()
+
+        task = Task.objects.get(pk=sub_task.task.id)
+        check = True
+        for s in task.subtask_set.all():
+            if not s.is_complete:
+                check = False
+                break
+        if check:
+            task.is_complete = True
+            task.completed_date = end_time
+            task.modified_at = end_time
+            task.save()
+            
+        response = SubTaskDetailSerializer(sub_task).data
+        return Response(response, status=status.HTTP_200_OK)
+    
+    except jwt.exceptions.ExpiredSignatureError:
+        return Response({'message': '로그인 세션이 만료되었습니다. 다시 로그인해주세요.'},
+                        status=status.HTTP_401_UNAUTHORIZED)
+    
+    except jwt.exceptions.InvalidTokenError:
+        return Response({'message': '토큰이 올바르지 않습니다.'}, status=status.HTTP_401_UNAUTHORIZED)
