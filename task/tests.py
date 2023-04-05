25558b16580f7ac1f8a996a831be7f5b80953125
yeseong31 <yeseong31@naver.com>
2023-04-05 22:58:20 +0900

    chore: SubTask 초기 설정 변경

--- task/tests.py
+++ task/tests.py
@@ -14,7 +14,7 @@ class TestTask(APITestCase):
         self.team5 = Team.objects.create(name='땅이')  # 5번 팀
         self.team6 = Team.objects.create(name='해태')  # 6번 팀
         self.team7 = Team.objects.create(name='수피')  # 7번 팀
-
+        
         # 테스트 사용자
         self.user1 = User.objects.create_user(
             email='kim@test.com',
@@ -23,7 +23,7 @@ class TestTask(APITestCase):
         )
         self.user1.set_password('!Test123?')
         self.user1.save()
-
+        
         # 테스트 Task
         self.task1 = Task.objects.create(
             create_user=self.user1,
@@ -39,14 +39,14 @@ class TestTask(APITestCase):
         )
         
         # Task의 하위 업무로 등록된 테스트 SubTask
-        self.sub_task1 = SubTask.objects.create(team=self.team1, task=self.task2)
-        self.sub_task2 = SubTask.objects.create(team=self.team2, task=self.task2)
+        self.sub_task1 = SubTask.objects.create(team=self.team1, task=self.task2, is_complete=True)
+        self.sub_task2 = SubTask.objects.create(team=self.team2, task=self.task2, is_complete=True)
         self.sub_task3 = SubTask.objects.create(team=self.team3, task=self.task2)
-
+        
         # URL
         self.login_url = '/api/accounts/v1/login/'
         self.task_url = '/task/'
-
+        
         # Bearer Token
         self.token = self.client.post(
             self.login_url,
@@ -76,7 +76,7 @@ class TestTask(APITestCase):
         self.assertEqual(response.data['sub_task'][0]['team']['id'], self.team1.id)
         self.assertEqual(response.data['sub_task'][1]['team']['id'], self.team2.id)
         self.assertEqual(response.data['sub_task'][2]['team']['id'], self.team3.id)
-        
+    
     def create_task_without_filling_team_list(self):
         data = {
             'title': '테스트 업무',
@@ -91,7 +91,7 @@ class TestTask(APITestCase):
         )
         # print(response.data)
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-        
+    
     def test_create_task_without_login(self):
         data = {
             'title': '테스트 업무',
@@ -104,17 +104,17 @@ class TestTask(APITestCase):
             format='json'
         )
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
-        
+    
     def test_get_task(self):
         response = self.client.get(path=f'{self.task_url}{self.task1.id}/')
         # print(response.data)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        
+    
     def test_get_non_existent_task(self):
         response = self.client.get(path='/task/12345/')
         # print(response.data)
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
-        
+    
     def test_get_task_list(self):
         response = self.client.get(path=self.task_url)
         # print(response.data)
@@ -125,7 +125,7 @@ class TestTask(APITestCase):
         data = {
             'title': '수정된 테스트 업무',
             'content': '수정된 테스트 업무 설명입니다.',
-            'team_list': [self.team1.id, self.team6.id]  # 2, 3번 팀 삭제, 6번 팀 추가
+            'team_list': [self.team1.id, self.team2.id, self.team6.id]  # 3번 팀 삭제, 6번 팀 추가
         }
         response = self.client.put(
             path=f'{self.task_url}{self.task2.id}/',
@@ -141,8 +141,9 @@ class TestTask(APITestCase):
         self.assertEqual(response.data['create_user']['username'], 'kim')
         self.assertEqual(response.data['create_user']['team'], self.team1.id)
         self.assertEqual(response.data['sub_task'][0]['team']['id'], self.team1.id)
-        self.assertEqual(response.data['sub_task'][1]['team']['id'], self.team6.id)
-        
+        self.assertEqual(response.data['sub_task'][1]['team']['id'], self.team2.id)
+        self.assertEqual(response.data['sub_task'][2]['team']['id'], self.team6.id)
+    
     def test_update_task_without_login(self):
         data = {
             'title': '수정된 테스트 업무',
@@ -156,7 +157,7 @@ class TestTask(APITestCase):
         )
         # print(response.data)
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
-        
+    
     def test_update_task_contains_completed_sub_tasks(self):
         self.sub_task1.is_complete = True
         self.sub_task1.save()
@@ -178,7 +179,7 @@ class TestTask(APITestCase):
         self.sub_task1.save()
         
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-        
+    
     def test_update_task_for_non_author_user(self):
         user2 = User.objects.create_user(
             email='han@test.com',
