64eb2fc4f5c2a8382014b6eaf4b738476ec27eb2
yeseong31 <yeseong31@naver.com>
2023-04-05 17:27:42 +0900

    test: 회원가입 테스트

--- account/tests.py
+++ account/tests.py
@@ -1,36 +1,52 @@
-from django.test import TestCase
+from rest_framework import status
 from rest_framework.test import APIRequestFactory, APITestCase
 
 from account.models import User, Team
 
 
-class TeamModelTest(TestCase):
-    def test_default_values(self):
-        team = Team.objects.create(
-            name='테스트팀'
-        )
-        self.assertEqual(team.name, '테스트팀')
-
-
-class UserModelTest(APITestCase):
+class UserRegisterViewTest(APITestCase):
     def setUp(self):
         self.factory = APIRequestFactory()
-        self.register_url = '/auth/signup/'
-        self.login_url = '/auth/login/'
-        self.team = Team.objects.create(
-            name='테스트팀'
-        )
+        self.register_url = '/api/accounts/v1/signup/'
+        self.login_url = '/api/accounts/v1/login/'
+        self.team = Team.objects.create(name='테스트팀')  # 1번 팀
         self.user = User.objects.create_user(
-            email='user1@test.com',
-            username='user1',
-            team=self.team,
-            pw='!test@1234?',
+            email='kim@test.com',
+            username='kim',
+            team=self.team
         )
-
+        self.user.set_password('!Test123?')
+    
     def test_default_values(self):
-        self.assertEqual(self.user.email, 'user1@test.com')
-        self.assertEqual(self.user.username, 'user1')
+        self.assertEqual(self.user.email, 'kim@test.com')
+        self.assertEqual(self.user.username, 'kim')
         self.assertEqual(self.user.team, self.team)
-        self.assertEqual(self.user.check_password('!test@1234?'), True)
+        self.assertEqual(self.user.check_password('!Test123?'), True)
         self.assertEqual(self.user.is_active, True)
         self.assertEqual(self.user.is_admin, False)
+        self.assertEqual(self.register_url, '/api/accounts/v1/signup/')
+        self.assertEqual(self.login_url, '/api/accounts/v1/login/')
+    
+    def test_register(self):
+        email = 'han@test.com'
+        username = 'han'
+        team = self.team.id
+        pw = '!Test1234?'
+        
+        data = {
+            'email': email,
+            'username': username,
+            "team": team,
+            'pw': pw
+        }
+        
+        response = self.client.post(
+            self.register_url,
+            data=data,
+            format='json'
+        )
+        
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.data['user']['email'], email)
+        self.assertEqual(response.data['user']['username'], username)
+        self.assertEqual(response.data['user']['team'], team)
